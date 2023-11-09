4359d0cd0df5f05a7254dc596fc175ab153b4edd
prabhudasrajagoni <150319812+prabhudasrajagoni@users.noreply.github.com>
2023-11-09 12:21:09 +0530

    Update DragandDrop.java

--- src/test/java/Mouseactions/DragandDrop.java
+++ src/test/java/Mouseactions/DragandDrop.java
@@ -7,7 +7,7 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.interactions.Actions;
-
+// * this is an argument
 public class DragandDrop {
 
 	public static void main(String[] args) throws InterruptedException {
